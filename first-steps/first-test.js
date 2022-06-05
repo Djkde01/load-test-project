9500648f09be72afbeb9cd2a396a3533321892f6
Djkde01 <sergio.estrella@utp.edu.co>
2022-06-05 01:50:46 -0500

    changed VUs stages & endpoint in first steps

--- first-steps/first-test.js
+++ first-steps/first-test.js
@@ -1,5 +1,5 @@
 import http from "k6/http";
 
 export default function () {
-  let response = http.get("https://test-api.k6.io");
+  let response = http.get("https://api.escuelajs.co/api/v1/products");
 }
--- first-steps/options.js
+++ first-steps/options.js
@@ -2,10 +2,9 @@ import http from "k6/http";
 
 export const options = {
   stages: [
-    { duration: "10s", target: 10 },
-    { duration: "10s", target: 100 },
-    { duration: "10s", target: 100 },
-    { duration: "10s", target: 10 },
+    { duration: "60s", target: 100 },
+    { duration: "180s", target: 100 },
+    { duration: "60s", target: 0 },
   ],
 };
 
