447a48aac96959d16e0cca01759fd573b59d8840
Djkde01 <sergio.estrella@utp.edu.co>
2022-06-27 11:24:58 -0500

    Added example with counter metric

---
+++ custom-metrics/counter.js
@@ -0,0 +1,35 @@
+import http, { CookieJar } from "k6/http";
+import { Counter } from "k6/metrics";
+
+export const options = {
+  vus: 100,
+  duration: "30s",
+};
+
+const categoriesCounter = new Counter("called_categories");
+const usersCounter = new Counter("called_users");
+const productsCounter = new Counter("called_products");
+
+export default function () {
+  switch (Math.floor(Math.random() * 3)) {
+    case 0:
+      let categoriesResponse = http.get(
+        "https://api.escuelajs.co/api/v1/products"
+      );
+      categoriesCounter.add(1);
+      break;
+
+    case 1:
+      let usersResponse = http.get("https://api.escuelajs.co/api/v1/products");
+      usersCounter.add(1);
+      break;
+    case 2:
+      let productsResponse = http.get(
+        "https://api.escuelajs.co/api/v1/products"
+      );
+      productsCounter.add(1);
+      break;
+    default:
+      break;
+  }
+}
