14ba18441c75d73b6797de99112da44ffd7ce52d
Djkde01 <sergio.estrella@utp.edu.co>
2022-08-17 22:49:54 -0500

    Added stages file and modified VUs in options

--- first-steps/stages.js
+++ first-steps/stages.js
@@ -1,13 +1,13 @@
-import { sleep } from "k6";
 import http from "k6/http";
 
 export const options = {
-  vus: 10,
-  duration: "30s",
+  stages: [
+    { duration: "60s", target: 100 },
+    { duration: "180s", target: 100 },
+    { duration: "60s", target: 0 },
+  ],
 };
 
 export default function () {
   let response = http.get("https://api.escuelajs.co/api/v1/products");
-
-  sleep(1);
 }
